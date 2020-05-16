d00d184ddff0accb3ac9c9dda7139d907f1c117a
christ0208 <christopherlimawan@gmail.com>
2020-05-16 18:11:13 +0700

    Added Search Forum

--- Member/search_forum.php
+++ Member/search_forum.php
@@ -5,16 +5,59 @@
     require_once 'middleware/logged_in_middleware.php';
 
     require_once 'navigation_bar.php';
-    
+
+    if(isset($_GET['search'])) {
+        $forumsQuery = "SELECT * FROM forums WHERE title='${_GET['searchQuery']}'";
+        $forumsResult = $conn->query($forumsQuery);
+    }
 ?>
 
 <link rel="stylesheet" href="public/css/bootstrap.min.css">
 
-<div class="container">
+<div class="container p-3">
     <form action="" class="d-flex">
         <input type="text" name="searchQuery" id="txtSearchQuery" class="form-control flex-fill">
-        <button class="btn btn-primary">
+        <button class="btn btn-primary" name="search" type="submit">
             Search
         </button>
     </form>
+    <?php
+        if(isset($_GET['search'])) {
+            ?>
+            <div>
+                Search Keyword: <?= $_GET['searchQuery'];?>
+            </div>
+            <table class="table table-hover mt-2">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if($forumsResult->num_rows === 0) {
+            ?>
+            <tr>
+                <td>No Forum Found</td>
+            </tr>
+            <?php
+        } else {
+            while($forumResult = $forumsResult->fetch_assoc()) {
+                ?>
+                <tr>
+                    <td>
+                        <a href="detail_forum.php?id=<?= $forumResult['id'];?>"><?= $forumResult['title'];?></a>
+                        <br>
+                        Created: <?= $forumResult['created_at'];?>
+                    </td>
+                </tr>
+                <?php
+            }
+        }
+        ?>
+        </tbody>
+    </table>
+            <?php
+        }
+    ?>
 </div>
