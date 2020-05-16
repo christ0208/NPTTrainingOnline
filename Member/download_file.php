19e3395d9ea8587e5f0bb9e6aed6fff28a185f94
christ0208 <christopherlimawan@gmail.com>
2020-05-16 19:46:58 +0700

    Added Download_file PHP for SSRF PoC

--- Member/download_file.php
+++ Member/download_file.php
@@ -1,3 +1,9 @@
 <?php
-    $filename = basename($_GET['path']);
-    file_put_contents($filename, fopen($_GET['path'], 'r'));
+$url = $_GET['path'];
+$filename = basename($url);
+$ch = curl_init($url);
+$fp = fopen("./$filename", "wb");
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_exec($ch);
+fclose($fp);
